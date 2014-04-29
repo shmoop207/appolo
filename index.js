eb06ed9e95dd89bcc65d1815d33f19bda0250771
roman <shmoop207@gmail.com>
2014-04-29 16:05:44 +0300

    new appolo

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
-module.exports.Class = require('appolo-class');
-module.exports.Util = require('./lib/util/util');
-module.exports.Controller = require('./lib/controller/controller');
-module.exports.EventDispatcher = require('./lib/events/event-dispatcher');
+module.exports = {
+    Class : require('appolo-class'),
+    Util : require('./lib/util/util'),
+    EventDispatcher : require('./lib/events/event-dispatcher'),
+    inject : require('./lib/inject/inject'),
+    loader : require('./lib/loader/loader'),
+    launcher : require('./lib/launcher/launcher'),
+    environment : require('./lib/environments/environments'),
+    _ : require('lodash')
+}
 
 
-module.exports.inject = require('./lib/inject/inject');
-
-module.exports.router = require('./lib/routes/router');
-
-module.exports.loader = require('./lib/loader/loader');
-
-module.exports.launcher = require('./lib/launcher/launcher');
-
-module.exports.environment = require('./lib/environments/environments');
-
-module.exports._ = require('lodash');
 
