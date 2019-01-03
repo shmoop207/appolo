a14a2260a166f70a772c3b4ab275e07fc0436bcd
roman <shmoop207@gmail.com>
2019-01-03 13:48:09 +0200

    agent bump

--- lib/interfaces/IOptions.ts
+++ lib/interfaces/IOptions.ts
@@ -1,7 +1,8 @@
 import {IOptions as IEngineOptions} from 'appolo-engine';
+import {IOptions as IAgentOptions} from 'appolo-agent';
 import { ValidationOptions } from 'joi';
 
-export interface IOptions extends IEngineOptions {
+export interface IOptions extends IEngineOptions,IAgentOptions {
 
     startMessage?: string,
     startServer?: boolean,
@@ -15,6 +16,7 @@ export interface IOptions extends IEngineOptions {
     urlParser?: "url" | "fast"
     viewFolder?: string
     viewCache?: boolean
+    trustProxy?:boolean
     viewEngine?: (path: string, options?: { cache?: boolean, [otherOptions: string]: any }) => Promise<string>;
     viewExt?: string
     ssl?: {
